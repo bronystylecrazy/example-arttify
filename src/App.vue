3877ced1b1b598a8fb6f7aac6f8621bba21c7e30
Sirawit Pratoomsuwan <48322519+bronystylecrazy@users.noreply.github.com>
2022-06-07 09:27:21 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Sirawit!" />
+  <HelloWorld msg="Hello Again Ajarn!!" />
 </template>
 
 <style>
